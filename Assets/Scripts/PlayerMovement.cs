0ec7f5711b46acaf47e3b52dcb95c835d46ebd99
GMonesma <73101404+GMonesma@users.noreply.github.com>
2020-10-22 12:25:32 +0200

    RunBudRun Commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int Speed = 5;
+    private CharacterController Player;
+    private Vector3 DirectionDeplacement;
+    public int Sensisbility;
+    public int Jump = 5;
+    public int Gravity = 20; 
+    
+    void Start()
+    {
+        Player = GetComponent<CharacterController>();
+    }
+
+
+    private void Update()
+    {
+        DirectionDeplacement.x = Input.GetAxisRaw("Vertical");
+        DirectionDeplacement.z = Input.GetAxisRaw("Horizontal");
+        DirectionDeplacement = transform.TransformDirection(DirectionDeplacement);
+
+        //Deplacement CHaracter Clavier
+        Player.Move(DirectionDeplacement * Time.deltaTime * Speed);
+
+        //Rotation Souris
+        transform.Rotate(0, Input.GetAxisRaw("Mouse X")*Sensisbility,0);
+
+        //Saut
+        if (Input.GetKeyDown(KeyCode.Space) && Player.isGrounded)
+        {
+            DirectionDeplacement.y = Jump;
+        }
+        //Gravité
+        if (!Player.isGrounded)
+        {
+            DirectionDeplacement.y -= Gravity * Time.deltaTime;  
+        }
+    }
+}
+ 
---
+++ Assets/Scripts/SimpleMove.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMove : MonoBehaviour
+{
+    
+    public Transform target;
+    public float vitesse = 1.0f;
+    private Vector3 randomTargetPos;
+    public float randomTargetDistMin = 2;
+    public float randomTargetDistMax = 10;
+    public bool useRandomTarget = false;
+    public int Sensisbility;
+    private Vector3 DirectionDeplacement;
+    
+
+    void Start ()
+    {
+        SetRandomTargetPos();
+    }
+
+    void SetRandomTargetPos()
+    {
+        randomTargetPos = Random.onUnitSphere;
+        randomTargetPos.y = 0;
+        randomTargetPos = randomTargetPos.normalized * Random.Range(randomTargetDistMin, randomTargetDistMax);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+            useRandomTarget = true;
+
+        Vector3 targetPos;
+        if (useRandomTarget || target == null)
+            targetPos = randomTargetPos;
+        else
+            targetPos = target.position;
+
+        Vector3 deplacement = target.position - transform.position;
+        deplacement = deplacement.normalized * vitesse * Time.deltaTime;
+        transform.position += deplacement;
+
+        transform.LookAt(target);
+
+    }
+} 
