04d165c11d2f6d60b4f059fe1cfdc613624d4a22
GMonesma <73101404+GMonesma@users.noreply.github.com>
2020-12-31 12:26:17 +0100

    Commit add obtacle

--- Assets/KillPlayer.cs
+++ Assets/KillPlayer.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class KillPlayer : MonoBehaviour
 {
+
+    private GameObject OpenPanel = null;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(other.gameObject);
+            OpenPanel.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            OpenPanel.SetActive(false);
+        }
+            
+    }
+
+    private bool IsOpenPanelActive
+    {
+        get
+        {
+            return OpenPanel.activeInHierarchy;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (IsOpenPanelActive)
+        {
+            OpenPanel.SetActive(false);
+        }
     }
 }
